3e23e6707e53351a8cea874f9318d0001e7fe000
blizarre <git@marache.net>
2016-08-10 22:21:26 +0100

    bugfix, code cleanup

--- euler10/solve.cpp
+++ euler10/solve.cpp
@@ -13,10 +13,7 @@ template <typename T> class PrimeGenerator {
   T next_number;
 
 public:
-
-  PrimeGenerator() {
-      next_number = 0;
-  }
+  PrimeGenerator() { next_number = 0; }
 
   T next() {
     T prime;
@@ -44,39 +41,47 @@ protected:
       if (number % *it == 0) {
         return false;
       }
-      it ++;
+      it++;
     }
     return true;
   }
 };
 
-
-uint64_t sumOfPrimesBelow(const int max_prime, const bool verbose) {
+uint64_t sumOfPrimesBelow(const uint64_t max_prime, const bool verbose) {
+  if (verbose) {
+    cout << endl;
+  }
   PrimeGenerator<uint64_t> generator;
   uint64_t sum_primes = 0;
   uint64_t count_primes = 0;
   uint64_t next_prime = generator.next();
 
-  while(next_prime < max_prime) {
+  while (next_prime < max_prime) {
     sum_primes += next_prime;
     next_prime = generator.next();
-    if(verbose && count_primes % 50000 == 0) {
-        cout << "\033[A\033[K" <<  (int)( 100 * next_prime / max_prime) << "%" << endl;
+    if (verbose && count_primes % 20000 == 0) {
+      cout << "\033[A\033[K" << (int)(100 * next_prime / max_prime) << "% ("
+           << count_primes << ")" << endl;
     }
-    count_primes ++;
+    count_primes++;
   }
   return sum_primes;
 }
 
-uint64_t sumOfNbPrimes(const int nb_primes, const bool verbose) {
+uint64_t sumOfNbPrimes(const uint64_t nb_primes, const bool verbose) {
+  if (verbose) {
+    cout << endl;
+  }
   uint64_t sum_primes = 0;
 
   PrimeGenerator<uint64_t> generator;
   for (uint64_t count_primes = 0; count_primes < nb_primes; count_primes++) {
     uint64_t prime = generator.next();
-    sum_primes += (prime % 10'000'000);
-    if(verbose && count_primes % 50000 == 0) {
-        cout << "\033[A\033[K" <<  (int)( 100 * count_primes / nb_primes) << "%" << endl;
+    sum_primes += prime;
+    sum_primes %= 10'000'000u;
+    if (verbose && count_primes % 100000 == 0) {
+      cout << "\033[A\033[K" << (int)(100 * count_primes / nb_primes) << "% ("
+           << count_primes << ")" << endl;
     }
   }
   return sum_primes;
@@ -98,9 +103,10 @@ void test() {
 int main(int argc, char *argv[]) {
   test();
   cout << "Computing the sum of the primes < 2'000'000 (euler)" << endl;
-  cout << sumOfNbPrimes(2'000'000, true) << endl;
-  cout << "Computing the sum mod 7 of the  8,971,406 first primes (euler)" << endl;
-  cout << sumOfPrimesBelow(8'971'406, true) << endl;
+  cout << sumOfPrimesBelow(2'000'000, true) << endl << endl;
+  cout << "Computing the sum mod 7 of the  8,971,406 first primes (euler)"
+       << endl;
+  cout << sumOfNbPrimes(8'971'406, true) << endl;
   cout << "Computing the sum mod 7 of the 434'926 first primes (euler)" << endl;
   cout << sumOfNbPrimes(434'926, true) << endl;
 }
