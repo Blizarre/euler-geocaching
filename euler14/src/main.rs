5b24b444aaa4c307f1496a4c7ef1aa830eef3cc6
Blizarre <github@marache.net>
2016-09-18 22:12:15 +0100

    fmt + import cleanup

--- euler14/src/main.rs
+++ euler14/src/main.rs
@@ -1,7 +1,5 @@
 // https://projecteuler.net/problem=14
 
-use std::io::prelude::*;
-
 #[cfg(test)]
 mod test_solver {
     use super::Solver;
@@ -25,7 +23,7 @@ mod test_solver {
 
 #[derive(Debug, Clone)]
 struct Solver {
-    data: Vec<Option<usize>>
+    data: Vec<Option<usize>>,
 }
 
 impl Solver {
@@ -45,26 +43,26 @@ impl Solver {
         let mut hops = Vec::<usize>::with_capacity(100);
 
         let mut new_nb = nb;
-        while new_nb != 1 && ! self.is_in_cache(new_nb) {
+        while new_nb != 1 && !self.is_in_cache(new_nb) {
             hops.push(new_nb);
             new_nb = match new_nb % 2 {
                 0 => new_nb / 2,
                 1 => new_nb * 3 + 1,
-                _ => panic!("Math error")
+                _ => panic!("Math error"),
             };
-        };
+        }
         // found the end, fill the data array
         let mut hops_nb = match self.data[new_nb] {
             Some(x) => x as usize,
-            None    => 1
+            None => 1,
         };
         for i in hops.iter().rev() {
             hops_nb += 1;
-            //println!("Adding {} for hops {}", *i, hops_nb);
-            if *i < self.data.len()  {
+            // println!("Adding {} for hops {}", *i, hops_nb);
+            if *i < self.data.len() {
                 self.data[*i] = Some(hops_nb);
             }
-        };
+        }
         hops_nb
     }
 }
@@ -73,11 +71,11 @@ fn print_geo_result(mut nb: usize) {
     let mut index: usize = 1;
     println!("[{}] = {}", index, nb);
     while nb != 1 {
-        index += 1 ;
+        index += 1;
         nb = match nb % 2 {
             0 => nb / 2,
             1 => nb * 3 + 1,
-            _ => panic!("Math error")
+            _ => panic!("Math error"),
         };
         println!("[{}] = {}", index, nb);
     }
@@ -85,10 +83,10 @@ fn print_geo_result(mut nb: usize) {
 }
 
 fn main() {
-    let nb_max:usize = 10000000;
+    let nb_max: usize = 10000000;
     let mut max_nb_hops = 0;
     let mut max_index = 0;
-    let mut nb_hops:usize;
+    let mut nb_hops: usize;
 
     let mut solver = Solver::new(nb_max);
     for i in 1..nb_max {
@@ -98,6 +96,8 @@ fn main() {
             max_index = i
         }
     }
-    println!("Maximum number of hops is {} for number {}", max_nb_hops, max_index);
+    println!("Maximum number of hops is {} for number {}",
+             max_nb_hops,
+             max_index);
     print_geo_result(max_index);
 }
