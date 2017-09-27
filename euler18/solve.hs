e29d43f1d33a7286e9934bc238d6820a702712a0
Simon Marache-Francisco <git@marache.net>
2017-09-27 09:22:10 +0100

    euler18: added first version of solution in haskell

---
+++ euler18/solve.hs
@@ -0,0 +1,28 @@
+-- https://projecteuler.net/problem=67
+-- https://projecteuler.net/problem=18
+
+parsePyramid :: String -> [ [Integer] ]
+parsePyramid text = reverse $ map (\l -> map read $ words l) (lines text)
+
+solveLines :: [Integer] -> [Integer] -> [Integer]
+solveLines (b:c:xc) (a:xa) =
+    let newElement = a + max b c
+        in newElement : solveLines (c:xc) xa
+solveLines [_] [] = []
+
+
+solvePyramid :: [ [Integer] ] -> [ [Integer] ]
+solvePyramid (a : b : xs) = 
+    let currentLine = solveLines a b
+    in currentLine : solvePyramid (currentLine:xs)
+solvePyramid (a : xs) = []
+
+extractSolution :: [ [Integer] ] -> Integer
+extractSolution pyramid = head . last $ pyramid
+
+
+main :: IO ()
+main = do
+    text <- getContents
+    print . extractSolution . solvePyramid . parsePyramid $ text
+
